453966294547610e3a7e2b2ca675f2fac099857e
Kava Developers <mehul9921@gmail.com>
2024-03-13 23:20:55 +0530

    Update career.blade.php

--- resources/views/web/career.blade.php
+++ resources/views/web/career.blade.php
@@ -20,7 +20,7 @@
                     <p>Empower yourself with the multifaceted work environment and get the chance to be a part of a workforce. Our employees are our assets and we are always in need of good employees who are looking for challenging & worthwhile career.</p>
                     <p>At Mecoson Labs, we seek to discover and champion the latest, most promising advancements against the world’s greatest challenges in international business.</p>
 
-                    <a href="{{ url('current-jobs') }}" class="btn r-04 btn--theme hover--tra-white">We're Hiring</a>
+                    <a href="{{ url('current-jobs') }}" class="btn r-04 btn--theme">We're Hiring</a>
 
                 </div>
             </div>	<!-- END TEXT BLOCK -->	
